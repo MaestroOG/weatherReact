34b1df6832aab98b01a8fa446e45cab3bfb7a4dc
MaestroOG <muneeburrehman822@gmail.com>
2024-01-25 19:18:22 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [searchVal, setSearchVal] = useState("")
+  const [data, setData] = useState([]);
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const secondHalfOfUrl = "&units=metric&appid=dc54d4ba901aa8db78e5ab9bb5637dd7"
+
+  const handleChange = (event) => {
+    setSearchVal(event.target.value)
+  }
+
+  const handleClick = async () => {
+    const response = await fetch(url + searchVal + secondHalfOfUrl);
+    let data = await response.json()
+    setData(data);
+    console.log(data.status);
+    console.log("Clicked");
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="search">
+          <input type="text" placeholder='Search for city' onChange={handleChange} />
+          <button onClick={handleClick}>Search</button>
+        </div>
+        {
+          data.hasOwnProperty("weather") ? (
+            <div className="card">
+              <h1>{data.name}</h1>
+              <p>Temperature: {Math.floor(data.main.temp)}°C</p>
+              <p>Feels like: {Math.floor(data.main.feels_like)}°C</p>
+              <p>Humdity: {data.main.humidity}%</p>
+              <p>Wind Speed: {data.wind.speed}km/h</p>
+
+            </div>
+          ) :
+            (
+              <p>Weather details will appear here</p>
+            )
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
